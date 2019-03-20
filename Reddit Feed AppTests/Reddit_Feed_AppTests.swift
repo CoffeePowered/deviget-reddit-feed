01c608d83de5dd0db0256f0bb98d4e8f00c02074
Lucas <>
2019-03-20 09:21:41 -0300

    Initial commit

---
+++ Reddit Feed AppTests/Reddit_Feed_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Reddit_Feed_AppTests.swift
+//  Reddit Feed AppTests
+//
+//  Created by Lucas on 20/03/2019.
+//  Copyright © 2019 Lucas. All rights reserved.
+//
+
+import XCTest
+@testable import Reddit_Feed_App
+
+class Reddit_Feed_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
