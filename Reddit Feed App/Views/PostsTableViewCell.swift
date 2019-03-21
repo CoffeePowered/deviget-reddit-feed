ad37c03e2411e442ed0f684d5a0ca59ab94b49cc
Lucas <>
2019-03-21 12:23:00 -0300

    Added implementation for posts feed cells

---
+++ Reddit Feed App/Views/PostsTableViewCell.swift
@@ -0,0 +1,50 @@
+//
+//  PostsTableViewCell.swift
+//  Reddit Feed App
+//
+//  Created by Lucas on 21/03/2019.
+//  Copyright © 2019 Lucas. All rights reserved.
+//
+
+import UIKit
+
+struct PostCellViewModel {
+    let author: String?
+    let timeAgo: String?
+    let shouldShowDot: Bool
+    let title: String?
+    let comments: String?
+    let thumbnailURL: URL?
+}
+
+class PostsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var timeAgoLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var readMarkDotLabel: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var commentsLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func configure(with viewModel: PostCellViewModel) {
+        
+    }
+
+    @IBAction func didTapMainArea(_ sender: Any) {
+    }
+    
+    @IBAction func didTapDismiss(_ sender: Any) {
+    }
+    
+}
--- Reddit Feed App/Views/PostsViewController.swift
+++ Reddit Feed App/Views/PostsViewController.swift
@@ -10,9 +10,40 @@ import UIKit
 
 class PostsViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+}
 
+extension PostsViewController: PostsViewing {
+    func update() {
+    }
+    
+    func present(post: Post, usingSplitView: Bool) {
+    }
+    
+    func dismissCellAtIndex(_ index: Int) {
+    }
+    
+    func show(message: String, withTitle: String) {
+    }
+}
+
+extension PostsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        return cell ?? UITableViewCell()
+    }
+    
+    
 }
